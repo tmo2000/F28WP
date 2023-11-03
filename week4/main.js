cb9e5628908ecba52a88a7fb4817fa82df601acb
Temisola Mayomikun Olajide <tmo2000@hw.ac.uk>
2023-11-03 10:32:26 +0400

    update

--- week4/main.js
+++ week4/main.js
@@ -1,7 +1,18 @@
-var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'https://your-github-username.github.io/your-repositoryname/cities1.json');
-ourRequest.onload = function() {
-var ourData = JSON.parse(ourRequest.responseText);
-console.log(ourData[0]);
+
+var cityContainer = document.getElementById("city-info");
+var btn = document.getElementById("btn");
+btn.addEventListener("click", function(){
+
+    var ourRequest = new XMLHttpRequest();
+    ourRequest.open('GET', 'https://tmo2000.github.io/F28WP/week4/cities1.json');
+    ourRequest.onload = function() {
+    var ourData = JSON.parse(ourRequest.responseText);
+    console.log(ourData[0]);
 };
-ourRequest.send();
+    ourRequest.send();
+})
+
+function renderHTML(data){
+    var htmlString = "this is a test";
+    cityContainer.insertAdjacentHTML('beforeend' , htmlString);
+    }
