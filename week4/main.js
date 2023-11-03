2d06bbd36d03035e0c4b4ea58ea2b1fb931087e9
Temisola Mayomikun Olajide <tmo2000@hw.ac.uk>
2023-11-03 10:20:25 +0400

    parsing json

--- week4/main.js
+++ week4/main.js
@@ -1,6 +1,7 @@
 var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'https://tmo2000.github.io/F28WP/week4/cities1.json');
+ourRequest.open('GET', 'https://your-github-username.github.io/your-repositoryname/cities1.json');
 ourRequest.onload = function() {
-    console.log(ourRequest.responseText);
+var ourData = JSON.parse(ourRequest.responseText);
+console.log(ourData[0]);
 };
 ourRequest.send();
